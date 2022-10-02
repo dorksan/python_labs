08d43d2a98f53062b432b3ec577dd06c96ee6304
dorksan <alinchasaur@yandex.ru>
2022-10-03 01:13:42 +0300

    Initial commit

---
+++ slumpy_dogs/labTwo/task1.py
@@ -0,0 +1,8 @@
+# принимает матрицу, возвращает матрицу
+def matrixTransposition(list matrix):
+
+# принимает матрицу, возвращает матрицу
+def matrixMultiplication(list matrixOne, list MatrixTwo):
+
+# принимает матрицу, возвращает число
+def determineRankOfMatrix(list matrix):
