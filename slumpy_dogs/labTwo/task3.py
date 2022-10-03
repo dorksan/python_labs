6f8efb1158d3b040c65ae8323df8eba3af1510d0
Grikoss <grikosru@mail.ru>
2022-10-03 18:06:14 +0300

    done task 3

--- slumpy_dogs/labTwo/task3.py
+++ slumpy_dogs/labTwo/task3.py
@@ -1,3 +1,6 @@
+import numpy as np
+import timeit
+
 # принимает матрицу, возвращает матрицу(пустую, в случае ошибки)
 def findInverseMatrix(matrix):
     numOfRows = len(matrix)
@@ -35,3 +38,49 @@ def findInverseMatrix(matrix):
             returnMatrix[i][j] *= intermediateValue
 
     return returnMatrix
+
+def IsNotString(string):
+    try:
+        newString = int(string)
+        return True
+    except ValueError:
+        return False
+
+
+print("0 - выход из программы")
+print("1 - нахождение обратной матрицы")
+print("2 - тайм тест")
+operations = (0, 1, 2)
+IsNotExit = True
+while IsNotExit:
+    while True:
+        print("\nВведите номер необходимой команды: ")
+        operator = input()
+        if IsNotString(operator):
+            chooseYourCommand = int(operator)
+            if chooseYourCommand in operations:
+                break
+        print("Неправильный ввод команды")
+    match chooseYourCommand:
+        case 0:
+            IsNotExit = False
+            print("\nВы вышли из программы")
+        case 1:
+            a = [[4, 3, 2], [2, 1, -1], [3, 3, 2]]
+            b = findInverseMatrix(a)
+            print(b)
+        case 2:
+            matrix = [[4, 3, 2], [2, 1, -1], [3, 3, 2]]
+
+            startTime = timeit.default_timer()
+            np.linalg.inv(matrix)
+            endTimeNumpy = timeit.default_timer() - startTime
+
+            startTime = timeit.default_timer()
+            findInverseMatrix(matrix)
+            endTime = timeit.default_timer() - startTime
+
+            print("Время нахождения обратной матрицы с помощью собственной функции:")
+            print(endTime)
+            print("Время нахождения обратной матрицы с помощью функции библиотеки numpy:")
+            print(endTimeNumpy)
