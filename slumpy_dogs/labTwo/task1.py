b36b1320ea8a2f3456f7c49ea31b1c61f38df412
dorksan <alinchasaur@yandex.ru>
2022-10-03 18:43:37 +0300

    Add menu in task 1

--- slumpy_dogs/labTwo/task1.py
+++ slumpy_dogs/labTwo/task1.py
@@ -1,3 +1,36 @@
+def isInt(string):
+    try:
+        newString = int(string)
+        return True
+    except ValueError:
+        return False
+
+def matrixInput():
+    print('\n-------------ВВОД РАЗМЕРА МАТРИЦЫ-------------\n')
+    print('Введите число строк:')
+    while True:
+        rows = input()
+        if not(isInt):
+            print("Ошибка ввода, попробуйте еще раз")
+        else:
+            rows = int(rows)
+            break
+    print('Введите число столбцов:')
+    while True:
+        columns = input()
+        if not(isInt):
+            print("Ошибка ввода, попробуйте еще раз")
+        else:
+            columns = int(columns)
+            break
+    matrix = []
+    for i in range(rows):
+        matrix.append([0]*columns)
+    print('Введите все числа матрицы через ENTER:')
+    for i in range(rows):
+        for j in range(columns):
+            matrix[i][j] = int(input())
+    return matrix
 def matrixOutput(matrix):
     rows = len(matrix)
     columns = len(matrix[0])
@@ -89,3 +122,40 @@ def determineRankOfMatrix(matrix):
                 return 1
             
     return 0
+
+print("0 - выход из программы")
+print("1 - транспонирование")
+print("2 - умножение")
+print("3 - нахождение ранга")
+operations = (0, 1, 2, 3)
+IsNotExit = True
+while IsNotExit:
+    while True:
+        print("\nВведите номер необходимой команды: ")
+        operator = input()
+        if isInt(operator):
+            chooseYourCommand = int(operator)
+            if chooseYourCommand in operations:
+                break
+        print("Неправильный ввод команды")
+    match chooseYourCommand:
+        case 0:
+            IsNotExit = False
+            print("\nВы вышли из программы")
+        case 1:
+            matrixOne = matrixInput()
+            transposedMatrix = transposeMatrix(matrixOne)
+            print('\nТранспонированная матрица:')
+            matrixOutput(transposedMatrix)
+        case 2:
+            print('Введите первую матрицу:\n')
+            matrixOne = matrixInput()
+            print('Введите вторую матрицу:\n')
+            matrixTwo = matrixInput()
+            multiplicatedMatrix = multiplicateMatrix(matrixOne, matrixTwo)
+            print('\nРезультат умножения:')
+            matrixOutput(multiplicatedMatrix)
+        case 3:
+            matrixOne = matrixInput()
+            rank = determineRankOfMatrix(matrixOne)
+            print('\nРанг матрицы:', rank)
